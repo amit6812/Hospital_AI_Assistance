8f389f7eb2f9b37565e6bc478f75631ade70c95c
amit6812 <amitverma6812@gmail.com>
2026-03-21 01:24:44 +0530

    update index.html

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app = FastAPI(title="Hospital Voice Agent", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://hospital-handler-bucket.s3-website.eu-north-1.amazonaws.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
